4cb4b55405e491da3c30342314e8346a26256eb9
dewanwakilahmed <dewanwakilahmed@gmail.com>
2023-05-03 19:22:41 +0600

    Arrays & Tuples

--- index.ts
+++ index.ts
@@ -1,4 +1,21 @@
+// Basic Types
 let id: number = 5;
 let company: string = 'Traversy Media';
 let isPubished: boolean = true;
 let x: any = 'Hello';
+
+// Arrays
+let ids: number[] = [1, 2, 3, 4, 5];
+let arr: any[] = [1, true, 'Hello'];
+
+// Tuple
+let person: [number, string, boolean] = [1, 'Brad', true];
+
+// Tuple Array
+let employee: [number, string][];
+
+employee = [
+  [1, 'Brad'],
+  [2, 'John'],
+  [3, 'Jill'],
+];
