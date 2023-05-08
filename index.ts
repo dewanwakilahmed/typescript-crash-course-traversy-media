439421e005560f0d092ba27c902013bf7aa38b85
dewanwakilahmed <dewanwakilahmed@gmail.com>
2023-05-08 12:53:51 +0600

    Function Interface

--- index.ts
+++ index.ts
@@ -101,3 +101,11 @@ const user1: UserInterface = {
 
 type Point = number | string;
 const p1: Point = 1;
+
+// Function Interface
+interface MathFunc {
+  (x: number, y: number): number;
+}
+
+const add: MathFunc = (x: number, y: number): number => x + y;
+const sub: MathFunc = (x: number, y: number): number => x - y;
